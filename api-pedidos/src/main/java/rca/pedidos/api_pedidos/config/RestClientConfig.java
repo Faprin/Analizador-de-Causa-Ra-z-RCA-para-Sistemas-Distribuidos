185e57b33827cd223ab6122e42b93bbe8a2f37aa
faprin <81379933+Faprin@users.noreply.github.com>
2026-06-29 14:07:29 +0200

    fix: Chaos controller fix y POM actualizado para el RestClient timeout

--- api-pedidos/src/main/java/rca/pedidos/api_pedidos/config/RestClientConfig.java
+++ api-pedidos/src/main/java/rca/pedidos/api_pedidos/config/RestClientConfig.java
@@ -3,7 +3,10 @@ package rca.pedidos.api_pedidos.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -16,6 +19,10 @@ public class RestClientConfig {
 
         return builder
                 .baseUrl(urlInventario)
+                .requestFactory(new HttpComponentsClientHttpRequestFactory() {{
+                    setConnectTimeout(Duration.ofSeconds(3));
+                    setReadTimeout(Duration.ofSeconds(5));
+                }})
                 .requestInterceptor((request, body, execution) -> {
                     return execution.execute(request, body);
                 })
