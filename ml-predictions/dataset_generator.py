3da7f267d93ed6dba1313b5d08316a8267cc12e7
faprin <81379933+Faprin@users.noreply.github.com>
2026-07-01 09:43:13 +0200

    feature: script genera datasets

--- ml-predictions/dataset_generator.py
+++ ml-predictions/dataset_generator.py
@@ -1,5 +1,7 @@
+import time
 import requests
 import random
+import threading
 
 from urllib3 import request
 
@@ -17,12 +19,130 @@ def getToken():
             }
     )
 
+    # response.raise_for_status()
     return response.json().get("token")
 
 
-def request_normal():
+# ========================
+#   PETICIONES A SERVIDOR
+# ========================
+def request_normal(headers):
     calls = [
         lambda: requests.get(f"{url_inventario}/inventario", headers=headers),
         lambda: requests.get(f"{url_inventario}/inventario/1", headers=headers),
         lambda: requests.get(f"{url_pedidos}/pedidos", headers=headers),
     ]
+
+    fn = random.choice(calls)
+    response = fn()
+    print(f"[NORMAL] {response.request.method} {response.url} {response.status_code}")
+
+
+def request_error(headers):
+    calls = [
+        lambda: requests.post(
+            f"{url_inventario}/chaos/db-saturate",
+            headers = headers,
+            params={"segundos": 30}
+        ),
+
+        lambda: requests.post(
+            f"{url_inventario}/chaos/error",
+            headers = headers,
+            params={"type": "npe"}
+        ),
+
+        lambda: requests.post(
+            f"{url_inventario}/chaos/error",
+            headers = headers,
+            params={"type": "db"} # RuntimeError
+        )
+    ]
+
+    fn = random.choice(calls)
+    try:
+        response = fn()
+        print(f"[CHAOS] {response.request.method} {response.url} {response.status_code}")
+
+    except requests.exceptions.Timeout:
+        print(f"[CHAOS]   Timeout esperado al inyectar caos")
+    except requests.exceptions.ConnectionError as e:
+        print(f"[CHAOS]   ConnectionError: {e}")
+    except Exception as e:
+        print(f"[CHAOS]   Error: {e}")
+
+
+def request_cascade_error(headers):
+
+    # dejamos al servidor sin conexion 15 segundos la basse de datos desde un hilo diferente
+    threading.Thread(
+        target=lambda: requests.post(
+            f"{url_inventario}/chaos/db-saturate",
+            headers = headers,
+            params = {
+                "segundos": 30
+            }
+        )
+    ).start()
+
+    time.sleep(1)
+
+    print("[CASCADE] Creando pedido mientras inventario está saturado...")
+
+    try:
+        request = requests.post(
+            f"{url_pedidos}/pedidos",
+            headers = headers,
+            json = {
+                "clienteId": 1,
+                "productoId": 1,
+                "cantidad": 1,
+                "precioUnitario": 20
+            },
+            timeout=10
+        )
+    
+        if request.status_code == 500:
+            print(f"[CASCADE] Fallo en cascada confirmado {request.status_code}")
+        else:
+            print(f"[CASCADE] Pedido {request.status_code}")
+
+    except requests.exceptions.Timeout:
+        print("[CASCADE] Timeout de red en pedidos")
+    except Exception as e:
+        print(f"[CASCADE] Error: {e}")
+
+
+# ========================
+#   MAIN
+# ========================
+if __name__ == "__main__":
+    print("[ ] GENERANDO DATASET")
+
+    token = getToken()
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-type": "application/json"
+    }
+
+    iteration = 0
+
+    while iteration < 1000:    
+        roll = random.random()
+        iteration += 1
+
+        if roll < 0.6:
+            request_normal(headers=headers)
+        elif roll < 0.8: 
+            request_error(headers=headers)
+        else:
+            request_cascade_error(headers=headers)
+
+        time.sleep(random.uniform(0.2, 1.0)) # tiempo entre peticiones
+
+        if iteration == 100: 
+            print("\n[AUTH] Renovando token...")
+            token = getToken()
+            headers["Authorization"] = f"Bearer {token}"
+
