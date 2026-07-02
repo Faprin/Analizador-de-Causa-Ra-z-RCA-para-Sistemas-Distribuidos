48ab8d63834ceebebd79281563100be03bd272c4
faprin <81379933+Faprin@users.noreply.github.com>
2026-07-02 09:35:25 +0200

    feature: metodo que lo pasa a df y luego exporta en csv

--- ml-predictions/descarga_logs.py
+++ ml-predictions/descarga_logs.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 from datetime import datetime, timedelta
+import pandas as pd
 
 
 # ====================
@@ -13,9 +14,9 @@ JSON_PATH = os.path.join(OUTPUT_DIR, "logs_dataset.json")
 CSV_PATH  = os.path.join(OUTPUT_DIR, "logs_dataset.csv")
 
 
-# ====================
-#   Descarga y Parseo
-# ====================
+# =========================
+#   Descarga y Parseo JSON
+# =========================
 def descargar(horas_atras = 24, limite = 5000):
 
     tiempo_actual = datetime.utcnow()
@@ -51,6 +52,30 @@ def parsear_logs(raw):
                 pass  # ignora líneas que no son JSON
     return registros
 
+# =========================
+#   DATAFRAME .csv conversor
+# =========================
+def log_a_dataframe(logs):
+
+    df = pd.DataFrame(logs)
+    columnas = [
+        "@timestamp", "service", "level",
+        "event_type", "outcome",
+        "http_method", "http_uri", "http_status", "duration_ms",
+        "error_type", "error_message", "error_origin",
+        "traceId"
+    ]
+
+    columnas_presentes = [c for c in columnas if c in df.colums]
+    df = df[columnas_presentes]
+
+    if "duration_ms" in df.columns:
+        df["duration_ms"] = pd.to_numeric(df["duration_ms"], errors = "coerce")
+    if "http_status" in df.columns:
+        df["http_status"] = pd.to_numeric(df["http_status"], error = "coerce")
+    
+    return df
+
 # ====================
 #   MAIN
 # ====================
@@ -62,5 +87,8 @@ if __name__ == "__main__":
 
     with open(JSON_PATH, "w") as f:
         json.dump(logs, f, indent=2)
+    
+    df = log_a_dataframe(logs)
+    df.to_csv(CSV_PATH, index=False)
 
     print("Guardado en logs_dataset.json")
